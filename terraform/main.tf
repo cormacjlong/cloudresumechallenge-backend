48c0d68d318fad152f0c4bd2560e64fc58069f07
Cormac Long <cormacjlong@gmail.com>
2024-03-27 14:45:33 +0000

    Update role assignments for Function App to access Storage Account

--- terraform/main.tf
+++ terraform/main.tf
@@ -69,13 +69,19 @@ resource "azurerm_linux_function_app" "func" {
 
 # Create a Role Assignment for the Function App to access the Storage Account
 resource "azurerm_role_assignment" "func_storage_role_assignment" {
-  scope = azurerm_storage_account.sa.id
-  role_definition_name = [
-    "Storage Blob Data Owner",
-    "Storage Account Contributor",
-    "Storage Queue Data Contributor"
-  ]
-  principal_id = azurerm_linux_function_app.func.identity.0.principal_id
+  scope                = azurerm_storage_account.sa.id
+  role_definition_name = "Storage Blob Data Owner"
+  principal_id         = azurerm_linux_function_app.func.identity.0.principal_id
+}
+resource "azurerm_role_assignment" "func_storage_role_assignment" {
+  scope                = azurerm_storage_account.sa.id
+  role_definition_name = "Storage Account Contributor"
+  principal_id         = azurerm_linux_function_app.func.identity.0.principal_id
+}
+resource "azurerm_role_assignment" "func_storage_role_assignment" {
+  scope                = azurerm_storage_account.sa.id
+  role_definition_name = "Storage Queue Data Contributor"
+  principal_id         = azurerm_linux_function_app.func.identity.0.principal_id
 }
 
 # Create CosmosDB account
