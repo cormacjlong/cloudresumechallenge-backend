44c2eb65b0fe0d82fa1b6df5639d4ff5d6edd53f
Cormac Long <cormacjlong@gmail.com>
2024-03-27 20:17:26 +0000

    Update workflow and remove unused code

--- api/function_app.py
+++ api/function_app.py
@@ -4,9 +4,6 @@ import azure.functions as func
 from azure.data.tables import TableServiceClient
 from azure.identity import DefaultAzureCredential
 
-@app.function_name(name="HttpTrigger1")
-@app.route(route="hello")
-
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request to increment the visitor count.')
 
