e7db3ad083262c5f078a1e19dbf52c034c708b9b
Cormac Long <cormacjlong@gmail.com>
2024-03-31 23:15:56 +0100

    Add variables for targets_resource_id and log_analytics_workspace_id in diagnostics module

--- terraform/modules/diagnostics/main.tf
+++ terraform/modules/diagnostics/main.tf
@@ -1,3 +1,11 @@
+variable "targets_resource_id" {
+  description = "(Required) The list of ID of an existing Resource on which to configure Diagnostic Settings. Changing this forces a new resource to be created."
+}
+
+variable "log_analytics_workspace_id" {
+  description = "(Required) Specifies the ID of a Log Analytics Workspace where Diagnostics Data should be sent."
+}
+
 data "azurerm_monitor_diagnostic_categories" "categories" {
   count       = length(var.targets_resource_id)
   resource_id = var.targets_resource_id[count.index]
