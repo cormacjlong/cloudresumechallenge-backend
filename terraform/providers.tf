b78e7d93094a0ba0adfeba8e0ebb3fdec75562b3
Cormac Long <cormacjlong@gmail.com>
2024-04-04 22:18:00 +0100

    Fix typo in backend key

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -16,7 +16,7 @@ terraform {
     resource_group_name  = "rg-platform-management"
     storage_account_name = "stterraformgj5f"
     container_name       = "terraform-state"
-    key                  = "crc-backendend.tfstate"
+    key                  = "crc-backend.tfstate"
     use_oidc             = true
   }
 }
