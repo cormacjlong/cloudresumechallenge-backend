c8d4181e32d8ce9a0b62bc1e3a123473fc7fc746
Cormac Long <cormacjlong@gmail.com>
2024-04-07 15:27:54 +0100

    Refactor API Management API configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -321,17 +321,14 @@ resource "azurerm_api_management" "apim" {
 #   }
 # }
 
-resource "azurerm_api_management_api" "this" {
-  name                = "example-api"
-  resource_group_name = azurerm_resource_group.rg.name
-  api_management_name = azurerm_api_management.apim.name
-  revision            = "1"
-  display_name        = "Example API"
-  path                = "example"
-  protocols           = ["https"]
+# resource "azurerm_api_management_api" "this" {
+#   name                = "example-api"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   api_management_name = azurerm_api_management.apim.name
+#   revision            = "1"
+#   display_name        = "Example API"
+#   path                = "example"
+#   protocols           = ["https"]
+#   subscription_required = false
 
-  import {
-    content_format = "swagger-link-json"
-    content_value  = "http://conferenceapi.azurewebsites.net/?format=json"
-  }
-}
+# }
