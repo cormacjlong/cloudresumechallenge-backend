096c0c1d8510f0e70bbdba5952a93635dc3732d8
Cormac Long <cormacjlong@gmail.com>
2024-04-14 14:28:17 +0100

    Update resource ID for APIM Gateway in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -342,7 +342,7 @@ resource "azurerm_api_management_api_operation_policy" "this" {
 # Get the Domain Ownership Identifier for the APIM Gateway
 resource "azapi_resource_action" "get_domain_ownership_identifier" {
   type                   = "Microsoft.ApiManagement/service@2022-08-01"
-  resource_id            = "https://management.azure.com/subscriptions/${data.azurerm_subscription.current.subscription_id}/providers/Microsoft.ApiManagement"
+  resource_id            = "/subscriptions/${data.azurerm_subscription.current.subscription_id}/providers/Microsoft.ApiManagement"
   action                 = "getDomainOwnershipIdentifier"
   response_export_values = ["*"]
 }
