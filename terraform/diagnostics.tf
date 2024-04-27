acd21ba1970e6da0822ecc88bdfc6f6c6cbbf7fd
Cormac Long <cormacjlong@gmail.com>
2024-04-27 17:49:42 +0100

    Update log_analytics_workspace_id and targets_resource_id in diagnostics.tf

--- terraform/diagnostics.tf
+++ terraform/diagnostics.tf
@@ -1,13 +1,13 @@
 module "config_diagnostics" {
   count                      = var.logging_on ? 1 : 0
   source                     = "./modules/diagnostics"
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.law[0].id
-  targets_resource_id = [azurerm_log_analytics_workspace.law[0].id,
-    azurerm_service_plan.asp.id,
-    azurerm_linux_function_app.func.id,
-    azurerm_storage_account.sa.id,
-    join("", [azurerm_storage_account.sa.id, "/blobServices/default"]),
-    join("", [azurerm_storage_account.sa.id, "/queueServices/default"]),
-    join("", [azurerm_storage_account.sa.id, "/tableServices/default"]),
-  join("", [azurerm_storage_account.sa.id, "/fileServices/default"])]
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.this[0].id
+  targets_resource_id = [azurerm_log_analytics_workspace.this[0].id,
+    azurerm_service_plan.this.id,
+    azurerm_linux_function_app.this.id,
+    azurerm_storage_account.this.id,
+    join("", [azurerm_storage_account.this.id, "/blobServices/default"]),
+    join("", [azurerm_storage_account.this.id, "/queueServices/default"]),
+    join("", [azurerm_storage_account.this.id, "/tableServices/default"]),
+  join("", [azurerm_storage_account.this.id, "/fileServices/default"])]
 }
