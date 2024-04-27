d7b4dd896663ad650952d9735d525c146c91322e
Cormac Long <cormacjlong@gmail.com>
2024-04-27 15:54:38 +0100

    Update GitHub Actions workflow and deployment steps

--- terraform/main.tf
+++ terraform/main.tf
@@ -106,14 +106,12 @@ resource "azurerm_role_assignment" "func_queuecontributor_storage_role_assignmen
 
 # Create CosmosDB account
 resource "azurerm_cosmosdb_account" "cosmosdb" {
-  name                      = module.naming.cosmosdb_account.name_unique
-  location                  = azurerm_resource_group.rg.location
-  resource_group_name       = azurerm_resource_group.rg.name
-  offer_type                = "Standard"
-  kind                      = "GlobalDocumentDB"
-  enable_automatic_failover = false
-  enable_free_tier          = true
-  minimal_tls_version       = "Tls12"
+  name                = module.naming.cosmosdb_account.name_unique
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+  minimal_tls_version = "Tls12"
   consistency_policy {
     consistency_level       = "BoundedStaleness"
     max_interval_in_seconds = 300
